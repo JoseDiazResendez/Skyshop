110f0fd4da5f51d7f7c66389587344f0b3ed7609
Peppo <al180074@alumnos.uacj.mx>
2023-10-29 18:37:42 -0600

    Busqueda del irvin

--- Skyshop/src/Routes/App.jsx
+++ Skyshop/src/Routes/App.jsx
@@ -3,7 +3,7 @@ export default function App() {
         <main>
             <h3 className="search"><input className='container' placeholder='Que deseas buscar'/></h3>
             <div className='video'>
-                <iframe width='860' height='500' src='https://www.youtube.com/embed/W7UA1ZJkEok?si=Hzalt4DAnQGwfOcx' title='YouTube video player' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowFullScreen></iframe>
+            <iframe width="900" height="500" src="https://www.youtube.com/embed/widZEAJc0QM?si=DBs9DRAwSolkRosr" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             </div>
             <h3 className="text">TODO LO QUE NECESITAS EN UN SOLO LUGAR</h3>
         </main>
