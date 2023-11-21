31845ba9cfca47240ab5e862eea3ed1eeff0c0b0
unknown <al180074@alumnos.uacj.mx>
2023-11-21 16:13:57 -0700

    ad

--- Skyshop/src/Routes/Busqueda.jsx
+++ Skyshop/src/Routes/Busqueda.jsx
@@ -1,12 +1,16 @@
 import { useLoaderData } from "react-router-dom"
+import './search.css'
 
 export function Busqueda() {
     let items = useLoaderData()
+    
+    
 
-    return (
-        items?.map(item => (
+    return ( 
+        items?.map(item => ( 
             <>
                 <div className="bodyBuscar">
+                    <div class="ad" > <a href = "https://www.callofduty.com/playnow/modernwarfare3?utm_source=webedia&utm_medium=paid&utm_campaign=brand"><img src="https://i.gyazo.com/ea212af7c28f3161b20434c31315e843.png" class = "mw3" /></a></div>
                     <ul key={item.titleItem} className="containerBuscar">
                         <div className="displayer">
                             <h2 className="no-pad">{item.titleItem}</h2>
