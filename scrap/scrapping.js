5eea8463393f3b95c6c84c045bcd564c2bd3d749
JoseDiazResendez <al180247@alumnos.uacj.mx>
2023-11-20 17:29:06 -0700

    Script arreglado

--- scrap/scrapping.js
+++ scrap/scrapping.js
@@ -6,7 +6,7 @@ const prompt = require("prompt-sync")()
 let data = []
 
 const shops = [
-    {
+    /* {
         vendor: 'Amazon',
         url: 'https://www.amazon.com.mx/s?k=',
         searchItem: async ({ browser, url, item }) => {
@@ -17,25 +17,60 @@ const shops = [
 
             const content = await page.$$eval('.a-size-mini.a-spacing-none.a-color-base > a', elements => elements.map(e => e.href))
 
+            for (let index = 0; index < 3; index++) {
+                await page.goto(content[index])
+
+                let titleItem = await page.$eval('.a-size-large.product-title-word-break', el => el.textContent)
+                let imgItem = await page.$eval('#landingImage', el => el.src)
+                let priceItem
+                try {
+                    priceItem = await page.$eval('.a-section.a-spacing-none.aok-align-center.aok-relative .a-price.aok-align-center.reinventPricePriceToPayMargin.priceToPay', el => el.textContent)
+                } catch (error) {
+                    priceItem = 'Precio no disponible'
+                }
+                //let rateItem = await page.$eval('.a-declarative .a-size-base.a-color-base', el => el.textContent)
+
+                const descriptionItem = {
+                    titleItem: titleItem,
+                    imgItem: imgItem,
+                    priceItem: priceItem,
+                    //rateItem: rateItem,
+                    urlItem: (content[index])
+                }
+
+                data.push(descriptionItem)
+            }
+
+            await page.close()
+        }
+    },*/
+    {
+        vendor: 'Sanborns',
+        url: 'https://www.sanborns.com.mx/resultados?query=',
+        searchItem: async ({ browser, url, item }) => {
+            let localItem = item.replace("%", "+")
+
+            const page = await browser.newPage()
+            await page.goto(url + localItem)
+
+            const content = await page.$$eval('.cardProduct > a', elements => elements.map(e => e.href))
+
             for (let index = 0; index < 5; index++) {
                 await page.goto(content[index])
 
-                let titleItem = await page.$eval('#title', el => el.textContent)
-                let imgItem = await page.$eval('#imgTagWrapperId > img', el => el.src)
+                let titleItem = await page.$eval('.stylesShopData_h2__7UK6k', el => el.textContent)
+                let imgItem = await page.$eval('.stylesSliderProductoDesktop_imagenActual__x9_VZ', el => el.src)
                 let priceItem
                 try {
-                    priceItem = await page.$eval('.aok-offscreen', el => el.textContent)
+                    priceItem = await page.$eval('.stylesDataPrice_pPrice__Go68G', el => el.textContent)
                 } catch (error) {
                     priceItem = 'Precio no disponible'
                 }
 
-                let rateItem = await page.$eval('#acrPopover .a-size-base.a-color-base', el => el.textContent)
-
                 const descriptionItem = {
-                    titleItem: titleItem.trim(),
+                    titleItem: titleItem,
                     imgItem: imgItem,
                     priceItem: priceItem,
-                    rateItem: rateItem.trim(),
                     urlItem: (content[index])
                 }
 
